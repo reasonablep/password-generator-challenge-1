999e54cffa45ccbffe2719490697e17e9ffae6a6
reasonablep <140934359+reasonablep@users.noreply.github.com>
2023-08-23 22:05:23 -0700

    Added checkboxes and password length entry box

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var generateBtn = document.querySelector("#letters");
+
 // Write password to the #password input
-function writePassword(length) {
-  var passwordText = generatePassword();
+function writePassword() {
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  for (var i=0; i<length; i++){
-    const charset="abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"'#''$''%&''()*+,-./:;<=>?@[\]^_`{|}~";
 
-    // How to get around "this" in definition of character set? //
+  constant 
 
-    const randomIndex = Math.floor(Math.random() * charset.length);
-    passwordText +=charset[randomIndex];
-  }
   
+//Random character generators//
+
+  function getLower () {
+    return String.fromCharCode (Math.floor(Math.random ()*26) + 97)
+    }
+
+  function getUpper () {
+      return String.fromCharCode (Math.floor(Math.random ()*26) + 65)
+      }
+
+  function getSymbol () {
+        return String.fromCharCode (Math.floor(Math.random ()*15) + 33)
+        }
+
   passwordText.value = password;
 
 }
