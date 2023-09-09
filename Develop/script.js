8bb76ba54cf46e9985713965bc3c7c42d1b1a1c6
reasonablep <140934359+reasonablep@users.noreply.github.com>
2023-09-08 18:48:31 -0700

    changed local file structure to make projects display alphabetically in finder.

--- Develop/script.js
+++ Develop/script.js
@@ -1,79 +1,67 @@
 // Assignment Code
+var generateBtn = document.querySelector("#generate");
+ 
+let passwordLength = window.prompt ("Enter password length");{
 
-// Code is not running properly, password does not generate, will turn in assignment and work with a tutor to clean it up for resubmit//
+if (passwordLength > 128)
+alert("Password length exceeds maxiumum allowed!")
 
-const passwordEl = document.getElementById('#password');
-const lengthEl = document.getElementById('length');
-const lowerEl = document.getElementById('lower');
-const upperEl = document.getElementById('upper');
-const numberEl = document.getElementById('number');
-const symbolEl = document.getElementById('symbol');
-const generateEl = document.getElementById("#generate");
+&& if (passwordLength < 8 )
+alert("Password length is less than minimum allowed!")
+return;
 
-const getCharacters = {
-  lower: getLower,
-  upper: getUpper,
-  number: getNumber,
-  symbol: getSymbol
+}
+
+let uppercase = window.confirm ("Do you wish to include uppercase letters?");
+let lowercase = window.confirm ("Do you wish to include lowercase letters?"); 
+let numbers = window.confirm ("Do you wish to include numbers?");
+let symbols = window.confirm ("Do you wish to include symbols?");
+
+let arr = [];
+
+function generatePassword () {
+let generatedPassword;
+generatedPassword = "Test";
+return generatedPassword;
+}
+
+for (let i=0; i<8; i++){
+  arr.push(length); 
+
+}
+for (i=0; i=>0; i++){
+  arr.push(uppercase);
+}
+
+for (i=0; i=>0; i++) {
+  arr.push(lowercase);
+} 
+
+for (i=0; i=>0; i++){
+  arr.push(numbers);
+}
+
+for (i=0; i=>0; i++) {
+  arr.push(symbols);
+}
+
+let charSets = {
+  lowercase: 'abcdefghijklmnopqrstuvwxyz',
+  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  numbers: '0123456789',
+  special: "!\"#$%&\"'()*+,-./:;<=>?@[\]^_`{|}~"
+}
+
+// value += charSet.charAt(Math.floor(Math.random() * charSet.length));
+// Write password to the #password input
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+  
 }
 
 // Add event listener to generate button
-generate.addEventListener("click", () => {
-  const length = +lengthEl.value;
-  const isLower = lowerEl.checked;
-  const isUpper = upperEl.checked;
-  const isNumber = numberEl.checked;
-  const isSymbol = symbolEl.checked;
-
-  passwordEl.innerText = generatePassword (length, isLower, isUpper, isNumber, isSymbol);
-
-});
-
-
-// // Write password to the #password input
-// function writePassword(){
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-// }
-
-function generatePassword(length, lower, upper, number, symbols) {
-  let generatedPassword = '';
-  const checkedCount = lower + upper + number + symbol;
-  const checkedArray = [{lower},{upper},{number},{symbol}].filter(item =>Object.values(item)[0]);
-
-
-  if (checkedCount === 0) {
-  return '';
-  }
-
-  for (let i=0; i<length; i+=checkedCount) {
-    checkedArray.forEach(type => {
-      const funcName = Object.keys(type)[0];
-      generatedPassword += getCharacters [funcName] ();
-    });
-  }
-
-  const finalPassword = generatedPassword.slice (0, length);
-
-  return finalPassword;
-}
-
-//Random character generators//
-
-function getLower () {
-  return String.fromCharCode (Math.floor(Math.random ()*26) + 97)
-  }
-
-function getUpper () {
-    return String.fromCharCode (Math.floor(Math.random ()*26) + 65)
-    }
-
-function getNumber () {
-  return String.fromCharCode (Math.floor(Math.random () *10) + 48)
-}
-
-function getSymbol () {
-      return String.fromCharCode (Math.floor(Math.random ()*15) + 33)
-      }
+generateBtn.addEventListener("click", writePassword);
