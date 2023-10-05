1c0a60d78f6dec1ffea08656442bbe59a4a21b5a
reasonablep <140934359+reasonablep@users.noreply.github.com>
2023-10-04 22:01:09 -0700

    Updated password generator JS after relearning JS basics

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
- 
-let passwordLength = window.prompt ("Enter password length");{
 
-if (passwordLength > 128)
+let charSets = {
+  lowercase: 'abcdefghijklmnopqrstuvwxyz',
+  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  numbers: '0123456789',
+  special: "!\"#$%&\"'()*+,-./:;<=>?@[\]^_`{|}~"
+}
+
+generateBtn.addEventListener('click', function () {
+
+let passwordLength = window.prompt ("Enter password length");
+
+if (passwordLength > 128) {
 alert("Password length exceeds maxiumum allowed!")
+return;
 
-&& if (passwordLength < 8 )
+} else if (passwordLength < 8 ) {
 alert("Password length is less than minimum allowed!")
 return;
 
@@ -17,51 +27,52 @@ let lowercase = window.confirm ("Do you wish to include lowercase letters?");
 let numbers = window.confirm ("Do you wish to include numbers?");
 let symbols = window.confirm ("Do you wish to include symbols?");
 
-let arr = [];
+let charSet = '';
 
-function generatePassword () {
-let generatedPassword;
-generatedPassword = "Test";
-return generatedPassword;
+if (uppercase) {
+  charSet += charSets.uppercase;
+
+} if (lowercase) {
+  charSet += charSets.lowercase;
+
+} if (numbers) {
+  charSet += charSets.numbers;
+ 
+} if (symbols) {
+  charSet += charSets.symbols;
+
+  } if (charSet.length === 0) {
+    alert('Please select a character set!');
+    return;
+
+  }
+    let generatedPassword = generatePassword(passwordLength, charSet);
+    writePassword(generatedPassword);
+
+});
+
+  function generatePassword(length, charSet) {
+  if (charSet.length === 0) {
+    return '';
+
+  }
+
+  let password = '';
+  for (let i=0; i < length; i++) {
+    let randomGenerate = Math.floor(Math.random() * charSet.length);
+      password += charSet.charAt(randomGenerate);
+  }
+
+  return password;
+
 }
 
-for (let i=0; i<8; i++){
-  arr.push(length); 
-
-}
-for (i=0; i=>0; i++){
-  arr.push(uppercase);
-}
-
-for (i=0; i=>0; i++) {
-  arr.push(lowercase);
-} 
-
-for (i=0; i=>0; i++){
-  arr.push(numbers);
-}
-
-for (i=0; i=>0; i++) {
-  arr.push(symbols);
-}
-
-let charSets = {
-  lowercase: 'abcdefghijklmnopqrstuvwxyz',
-  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  numbers: '0123456789',
-  special: "!\"#$%&\"'()*+,-./:;<=>?@[\]^_`{|}~"
-}
-
-// value += charSet.charAt(Math.floor(Math.random() * charSet.length));
 // Write password to the #password input
 
-function writePassword() {
-  var password = generatePassword();
+  
+  function writePassword(password) {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  }
